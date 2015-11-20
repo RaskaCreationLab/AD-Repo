91f818f0c09ef795546d02092091f1722900cc9e
MrLowbob <ralf-von-der-reith@gmx.de>
2015-11-20 16:38:01 +0100

    save

--- AlguDat/src/Implementations/InsertionSort.java
+++ AlguDat/src/Implementations/InsertionSort.java
@@ -4,21 +4,19 @@ import Interfaces.AdtArray;
 
 public class InsertionSort {
 
-	private InsertionSort() {
+	private InsertionSort() {}
+	
+	public static void insertionSort(AdtArray array, int startIndex, int endIndex) {
 		
 	}
 	
-	public static void InsertionSort(AdtArray array, int startIndex, int endIndex) {
-		
-	}
-	
-	public static long InsertionSortRuntime(AdtArray array, int startIndex, int endIndex) {
+	public static long insertionSortRuntime(AdtArray array, int startIndex, int endIndex) {
 		long startTime = System.currentTimeMillis();
-		InsertionSort(array, startIndex, endIndex);
+		insertionSort(array, startIndex, endIndex);
 		return System.currentTimeMillis() - startTime;
 	}
 
-	public static int[] InsertionSortAccessCount(AdtArray array, int startIndex, int endIndex) {
+	public static int[] insertionSortAccessCount(AdtArray array, int startIndex, int endIndex) {
 		int readArray = 0, writeArray = 0, readAll = 0, writeAll = 0;
 		int[] accesses = {readArray, writeArray, readAll, writeAll};
 		return accesses;
