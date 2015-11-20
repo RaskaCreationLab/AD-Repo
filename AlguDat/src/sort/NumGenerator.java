1c62e60a744cf61f43a76a8b7f0ace52256f1e63
MrLowbob <ralf-von-der-reith@gmx.de>
2015-11-20 17:33:40 +0100

    NumGenerator fertig aber nicht getestet

--- AlguDat/src/sort/NumGenerator.java
+++ AlguDat/src/sort/NumGenerator.java
@@ -1,7 +1,10 @@
 package sort;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 import adt.Interfaces.AdtArray;
@@ -12,17 +15,41 @@ public class NumGenerator {
 	private NumGenerator() {}
 	
 	public static void sortNum(String filename, int quantity) {
-		
+		try { 
+			FileWriter fw = new FileWriter(filename+".dat");
+			BufferedWriter bw = new BufferedWriter(fw);
+			Random rnd = new Random(System.currentTimeMillis());
+			for (int i = 0; i > quantity; i++) {
+				bw.write(rnd.nextInt(quantity)+" ");
+			}
+
+	    	bw.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void sortNum(String filename, int quantity, boolean desc) {
-		
+		try { 
+			FileWriter fw = new FileWriter(filename+".dat");
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			for (int i = 0; i < quantity && desc == false; i++) {
+				bw.write(i+" ");
+			}
+			for (int i = quantity; i > 0 && desc == true; i++) {
+				bw.write(i+" ");
+			}
+
+	    	bw.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static AdtArray readNum(String filename) {
 		AdtArray array = AdtArrayImpl.initA();
 		try (Scanner scanner = new Scanner(new File(filename+".dat"))) {
-			
 			int index = 0;
 			while(scanner.hasNextInt()) {
 				array.setA(index, scanner.nextInt());
